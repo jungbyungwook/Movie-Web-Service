4cbc10882c2e890d3a206136a50d045a8273b4bf
jungbyungwook <korjby400@gmail.com>
2022-04-01 19:52:20 +0900

    useState를 사용하여 해당 state 값이 초기화 될떄 익명함수 실행

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -4,11 +4,39 @@ import { useEffect, useState } from "react";
 
 function App() {
     const [counter, setCounter] = useState(0);
-    const onClick = () => setCounter((prev) => prev + 1);
-    console.log("i run all the time");
-    useEffect(() => console.log("call the API"), []);
+    const [keyword, setKeyword] = useState("");
+
+    const onClick = () => {
+        setCounter((prev) => prev + 1);
+    };
+    const onChange = (event) => {
+        setKeyword(event.target.value);
+    };
+
+    useEffect(() => {
+        console.log("i run only once.");
+    }, []);
+    useEffect(() => {
+        // if (keyword !== "" && keyword.length > 5) {
+        //     console.log(`Search for ${keyword}`);
+        // }
+        console.log("i run when 'keyword' changes.");
+    }, [keyword]);
+    useEffect(() => {
+        console.log("i run when 'counter' changes.");
+    }, [counter]);
+    useEffect(() => {
+        console.log("i run when keyword & counter changes.");
+    }, [keyword, counter]);
+
     return (
         <div>
+            <input
+                value={keyword}
+                onChange={onChange}
+                type="text"
+                placeholder="Seacrch here..."
+            />
             <h1 className={styles.title}>{counter}</h1>
             {/* <Button text="Continue" /> */}
             <button onClick={onClick}>Click me</button>
