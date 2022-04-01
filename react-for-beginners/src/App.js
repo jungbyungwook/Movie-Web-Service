e117640e7eafe869b358abab7eaf4b3b8ca49b75
jungbyungwook <korjby400@gmail.com>
2022-04-01 19:19:06 +0900

    useState를 사용하여 첫렌더링시에만 익명함수 실행

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,10 +1,17 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+
 function App() {
+    const [counter, setCounter] = useState(0);
+    const onClick = () => setCounter((prev) => prev + 1);
+    console.log("i run all the time");
+    useEffect(() => console.log("call the API"), []);
     return (
         <div>
-            <h1 className={styles.title}>Welcome back!!!</h1>
-            <Button text="Continue" />
+            <h1 className={styles.title}>{counter}</h1>
+            {/* <Button text="Continue" /> */}
+            <button onClick={onClick}>Click me</button>
         </div>
     );
 }
