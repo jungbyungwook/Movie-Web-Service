e749abd2fbc99cef324d1a96097d74914cbe0134
jungbyungwook <korjby400@gmail.com>
2022-04-02 09:39:35 +0900

    Coin Tracker 만들기

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -3,41 +3,62 @@ import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
 function App() {
-    const [toDo, setToDo] = useState("");
-    const [toDos, setToDos] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    const [money, setMoney] = useState(0);
 
-    const onChange = (event) => {
-        setToDo(event.target.value);
-    };
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if (toDo === "") {
-            return;
-        }
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json);
+                setLoading(false);
+            });
+        console.log("on useEffect");
+    }, []);
 
-        setToDos((currentArray) => [toDo, ...currentArray]);
-        setToDo("");
+    const onBlur = (event) => {
+        setMoney(event.target.value);
     };
-    console.log(toDos);
+
+    const onClickBtn = (event) => {
+        console.log(money);
+    };
+
+    // function
+
     return (
         <div>
-            <h1>My To Dos ({toDos.length}) </h1>
-            <form onSubmit={onSubmit}>
-                <input
-                    onChange={onChange}
-                    value={toDo}
-                    type="text"
-                    placeholder="Write your to do..."
-                />
-                <button>Add To Do</button>
-            </form>
-            <hr />
+            <h1>The Coins! {loading ? "" : `(${coins.length})`} </h1>
 
-            <ul>
-                {toDos.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
+            <div>
+                <input onBlur={onBlur} placeholder="금액을 입력해주세요." />
+                <button onClick={onClickBtn}>입력</button>
+            </div>
+
+            {loading ? (
+                <strong>Loading...</strong>
+            ) : (
+                <ol>
+                    {coins
+                        .filter((coin) => money >= coin.quotes.USD.price)
+                        .map((coin) => (
+                            <li key={coin.id}>
+                                {coin.name} ({coin.symbol}) : $
+                                {coin.quotes.USD.price.toFixed(2)} USD{" "}
+                                {(money / coin.quotes.USD.price).toFixed(2)}{" "}
+                                개를 가질수있습니다.
+                            </li>
+                        ))}
+
+                    {/* {coins.map((coin) => (
+                    <option key={coin.id}>
+                        {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}{" "}
+                        USD
+                    </option>
+                ))} */}
+                </ol>
+            )}
         </div>
     );
 }
