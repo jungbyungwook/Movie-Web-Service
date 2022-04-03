9c003133707569e457d69f9e2b7d96991f5c5429
jungbyungwook <korjby400@gmail.com>
2022-04-04 07:19:17 +0900

    Movie image, title, summary, genres 보여주기

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,63 +1,46 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 
 function App() {
     const [loading, setLoading] = useState(true);
-    const [coins, setCoins] = useState([]);
-    const [money, setMoney] = useState(0);
+    const [movies, setMovies] = useState();
+
+    const getMovies = async () => {
+        const json = await (
+            await fetch(
+                `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+            )
+        ).json();
+
+        setMovies(json.data.movies);
+        setLoading(false);
+    };
 
     useEffect(() => {
-        fetch("https://api.coinpaprika.com/v1/tickers")
-            .then((response) => response.json())
-            .then((json) => {
-                setCoins(json);
-                setLoading(false);
-            });
-        console.log("on useEffect");
+        getMovies();
     }, []);
 
-    const onBlur = (event) => {
-        setMoney(event.target.value);
-    };
-
-    const onClickBtn = (event) => {
-        console.log(money);
-    };
-
-    // function
-
+    console.log(movies);
     return (
         <div>
-            <h1>The Coins! {loading ? "" : `(${coins.length})`} </h1>
-
-            <div>
-                <input onBlur={onBlur} placeholder="금액을 입력해주세요." />
-                <button onClick={onClickBtn}>입력</button>
-            </div>
-
             {loading ? (
-                <strong>Loading...</strong>
+                <h1>Loading...</h1>
             ) : (
-                <ol>
-                    {coins
-                        .filter((coin) => money >= coin.quotes.USD.price)
-                        .map((coin) => (
-                            <li key={coin.id}>
-                                {coin.name} ({coin.symbol}) : $
-                                {coin.quotes.USD.price.toFixed(2)} USD{" "}
-                                {(money / coin.quotes.USD.price).toFixed(2)}{" "}
-                                개를 가질수있습니다.
-                            </li>
-                        ))}
-
-                    {/* {coins.map((coin) => (
-                    <option key={coin.id}>
-                        {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}{" "}
-                        USD
-                    </option>
-                ))} */}
-                </ol>
+                <div>
+                    {movies.map((movie) => (
+                        <div key={movie.id}>
+                            <img src={movie.medium_cover_image}></img>
+                            <h2>{movie.title}</h2>
+                            <p>{movie.summary}</p>
+                            <ul>
+                                {movie.genres.map((g) => (
+                                    <li key={g}>{g}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
             )}
         </div>
     );
