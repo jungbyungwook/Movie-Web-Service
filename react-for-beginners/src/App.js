0359e59da6fe7074152e1653b004d0f31c87dfc6
jungbyungwook <korjby400@gmail.com>
2022-04-05 17:01:03 +0900

    React Router 사용

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,48 +1,15 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useEffect, useState, useMemo } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routers/Home";
+import Detail from "./routers/Detail";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState();
-
-    const getMovies = async () => {
-        const json = await (
-            await fetch(
-                `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-            )
-        ).json();
-
-        setMovies(json.data.movies);
-        setLoading(false);
-    };
-
-    useEffect(() => {
-        getMovies();
-    }, []);
-
-    console.log(movies);
     return (
-        <div>
-            {loading ? (
-                <h1>Loading...</h1>
-            ) : (
-                <div>
-                    {movies.map((movie) => (
-                        <div key={movie.id}>
-                            <img src={movie.medium_cover_image}></img>
-                            <h2>{movie.title}</h2>
-                            <p>{movie.summary}</p>
-                            <ul>
-                                {movie.genres.map((g) => (
-                                    <li key={g}>{g}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    ))}
-                </div>
-            )}
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/movie" element={<Detail />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
